d8eb298496173968d9f6d19c29c3b0ffb33b3db5
bythealphabet <bythealphabet@gmail.com>
2021-04-02 16:52:55 -0400

    first commit

---
+++ client/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+function App(props) {
+	return <div>App this add great that it works just like I want to</div>;
+}
+
+export default hot(App);
